5637d43f185728bcda53ea5243479aee47c6e225
shaotao1988 <shaotao1988@gmail.com>
2017-05-10 21:13:17 -0500

    Revert "finish parser function"
    This reverts commit eaafd2561cc4c4f5b322a7cb6051f91cdc3e71a5.

--- KmlParser.py
+++ KmlParser.py
@@ -1,57 +1,48 @@
 import xml.sax
-import openpyxl
 
 class KmlHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.content = ""
         self.site_name = ""
-        self.site_coordinate = ""
-        self.site_list = []
-        self.in_placemark = 0
-
-    def write_to_excel(self):
-        wb = openpyxl.Workbook()
-        ws = wb.active
-        ws.cell(1, 1).value = "Site Name"
-        ws.cell(1, 2).value = "Longtitude"
-        ws.cell(1, 3).value = "Latitude"
-        for site in self.site_list:
-            ws.append(site)
-        wb.save("test.xlsx")
+        self.coordinate = ""
+        self.longtitude = ""
+        self.altitude = ""
+        self.flag = 0
+        print("####")
 
     def startElement(self, tag, attributes):
         self.content = tag
-        # print("Start tag:" + tag)
+        print("Start tag:" + tag)
         if tag == "Placemark":
-            self.in_placemark = 1
+            self.flag = 1
 
     def endElement(self, tag):
-        # print("End tag:" + tag)
+        print("End tag:" + tag)
         if tag == "Placemark":
-            co_list = self.site_coordinate.split(",")
-            self.site_list.append([self.site_name, co_list[0], co_list[1]])
+            #print(self.site_name)
+            #print(self.coordiante)
             self.site_name = ""
             self.site_coordinate = ""
-            self.in_placemark = 0
-        elif tag == "kml":
-            self.write_to_excel()
+            self.flag = ""
             return
 
     def characters(self, content):
-        if self.in_placemark == 0 or content.strip() == "":
+        if self.flag == 0 or content.strip() == "":
             return
         elif self.content == "name":
-            self.site_name = content
+            self.site_name += content
+            print("name:" + self.site_name)
         elif self.content == "coordinates":
-            self.site_coordinate = content
+            self.coordinate += content
+            print("coordinates:" + self.coordinate)
         else:
             return
 
-
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     parser = xml.sax.make_parser()
+    #parser.setFeature(xml.sax.handler.feature_namespace, 0)
 
     Handler = KmlHandler()
     parser.setContentHandler(Handler)
 
-    parser.parse("test/test.xml")
+    parser.parse("test.xml")
